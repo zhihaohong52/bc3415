b0197bfb0395b7c6d5e4d21b520095c3d8324087
James <84340802+zhihaohong52@users.noreply.github.com>
2024-08-20 15:05:46 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,19 @@ app = Flask(__name__)
 def index():
   return(render_template("index.html"))
 
+@app.route("/financial_QA", methods = ["GET", "POST"])
+def financial_QA():
+  return(render_template("financial_qa.html"))
+
+@app.route("/makersuite", methods = ["GET", "POST"])
+def makersuite():
+  q = request.form.get("q")
+  
+  return(render_template("makersuit.html"), r=r.last)
+
+@app.route("/prediction", methods = ["GET", "POST"])
+def index():
+  return(render_template("prediction.html"))
+
 if __name__ == "__main__":
   app.run()
